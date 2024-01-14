7cdaf51d56def99f25f6de5ff49a8b904bbba119
Gabriel <gabrielbgarcia0999@gmail.com>
2024-01-13 22:08:30 -0300

    test(quest): delete handler unit tests

--- internal/controller/rest/quest_test.go
+++ internal/controller/rest/quest_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -161,6 +162,124 @@ func TestBuildBuildCreateQuestHanlder(t *testing.T) {
 		})
 
 		req := httptest.NewRequest(http.MethodPost, "/api/v1/quests", bytes.NewBufferString("{}"))
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set(gameIDHeader, gameID)
+
+		resp, err := app.Test(req)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+
+		var data ErrorResponse
+		err = json.NewDecoder(resp.Body).Decode(&data)
+		assert.NoError(t, err)
+
+		assert.Equal(t, ErrorResponseInternalServerError.Code, data.Code)
+		assert.Equal(t, ErrorResponseInternalServerError.Message, data.Message)
+	})
+}
+
+func TestBuildBuildDeleteQuestHanlder(t *testing.T) {
+	var (
+		questID = uuid.NewString()
+		gameID  = uuid.NewString()
+	)
+
+	t.Run("OK", func(t *testing.T) {
+		app := App(Config{
+			SoftDeleteQuestFunc: func(ctx context.Context, questID, gameID string) error {
+				return nil
+			},
+		})
+
+		req := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/api/v1/quests/%s", questID), nil)
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set(gameIDHeader, gameID)
+
+		resp, err := app.Test(req)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+	})
+
+	t.Run("Invalid Quest ID", func(t *testing.T) {
+		app := App(Config{
+			SoftDeleteQuestFunc: func(ctx context.Context, questID, gameID string) error {
+				return quest.ErrInvalidQuestID
+			},
+		})
+
+		req := httptest.NewRequest(http.MethodDelete, "/api/v1/quests/invalid-id", nil)
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set(gameIDHeader, gameID)
+
+		resp, err := app.Test(req)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusUnprocessableEntity, resp.StatusCode)
+
+		var data ErrorResponse
+		err = json.NewDecoder(resp.Body).Decode(&data)
+		assert.NoError(t, err)
+
+		assert.Equal(t, ErrorResponseQuestInvalidID.Code, data.Code)
+		assert.Equal(t, ErrorResponseQuestInvalidID.Message, data.Message)
+	})
+
+	t.Run("Missing Game ID", func(t *testing.T) {
+		app := App(Config{})
+
+		req := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/api/v1/quests/%s", questID), nil)
+
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := app.Test(req)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusUnprocessableEntity, resp.StatusCode)
+
+		var data ErrorResponse
+		err = json.NewDecoder(resp.Body).Decode(&data)
+		assert.NoError(t, err)
+
+		assert.Equal(t, ErrorResponseQuestInvalidGameID.Code, data.Code)
+		assert.Equal(t, ErrorResponseQuestInvalidGameID.Message, data.Message)
+	})
+
+	t.Run("Not Found", func(t *testing.T) {
+		app := App(Config{
+			SoftDeleteQuestFunc: func(ctx context.Context, questID, gameID string) error {
+				return quest.ErrQuestNotFound
+			},
+		})
+
+		req := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/api/v1/quests/%s", questID), nil)
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set(gameIDHeader, gameID)
+
+		resp, err := app.Test(req)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+
+		var data ErrorResponse
+		err = json.NewDecoder(resp.Body).Decode(&data)
+		assert.NoError(t, err)
+
+		assert.Equal(t, ErrorResponseQuestNotFound.Code, data.Code)
+		assert.Equal(t, ErrorResponseQuestNotFound.Message, data.Message)
+	})
+
+	t.Run("Random Error", func(t *testing.T) {
+		zap.Start()
+		defer zap.Sync()
+
+		app := App(Config{
+			SoftDeleteQuestFunc: func(ctx context.Context, questID, gameID string) error {
+				return errors.New("any error")
+			},
+		})
+
+		req := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/api/v1/quests/%s", questID), nil)
 
 		req.Header.Set("Content-Type", "application/json")
 		req.Header.Set(gameIDHeader, gameID)
